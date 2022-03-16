adcb1e3f980d3216845a9b8ecebce019beba4452
Sarthak Bansal <sbansal793@gmail.com>
2022-03-16 19:13:27 +0530

    made changes to the home page

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,14 @@ export default function Home() {
   const [countries, setCountries] = useState({ features: [] });
   const [globeSize, setGlobeSize] = useState(400);
   const router = useRouter();
+  const N = 20;
+  const arcsData = [...Array(N).keys()].map(() => ({
+    startLat: (Math.random() - 0.5) * 180,
+    startLng: (Math.random() - 0.5) * 360,
+    endLat: (Math.random() - 0.5) * 180,
+    endLng: (Math.random() - 0.5) * 360,
+    color: ["#E32A34",'#B50EAE' ]
+  }));
 
   useEffect(() => {
     var cookie = require('cookie');
@@ -174,6 +182,11 @@ export default function Home() {
         <Row>
           <Col md={true} className={styles.globeDiv}>
             <Globe
+            arcsData={arcsData}
+            arcColor={'color'}
+            arcDashLength={() => Math.random()}
+            arcDashGap={() => Math.random()}
+            arcDashAnimateTime={() => Math.random() * 4000 + 500}
               width={globeSize}
               height={globeSize}
               backgroundColor="rgba(0,0,0,0)"
@@ -182,157 +195,159 @@ export default function Home() {
               hexPolygonResolution={3}
               hexPolygonMargin={0.3}
               hexPolygonColor={(x) => {
-                switch (x.properties.SOVEREIGNT) {
-                  case "India":
-                    return `#7ed957`;
-                    break;
-                  case "Russia":
-                    return `#7ed957`;
-                    break;
-                  case "France":
-                    return `#7ed957`;
-                    break;
-                  case "Belgium":
-                    return `#7ed957`;
-                    break;
-                  case "Spain":
-                    return `#7ed957`;
-                    break;
-                  case "Germany":
-                    return `#7ed957`;
-                    break;
-                  case "Poland":
-                    return `#7ed957`;
-                    break;
-                  case "Ireland":
-                    return `#7ed957`;
-                    break;
-                  case "Italy":
-                    return `#7ed957`;
-                    break;
-                  case "Greece":
-                    return `#7ed957`;
-                    break;
-                  case "Croatia":
-                    return `#7ed957`;
-                    break;
-                  case "South Africa":
-                    return `#7ed957`;
-                    break;
-                  case "Austria":
-                    return `#7ed957`;
-                    break;
-                  case "Switzerland":
-                    return `#7ed957`;
-                    break;
-                  case "Netherlands":
-                    return `#7ed957`;
-                    break;
-                  case "Norway":
-                    return `#7ed957`;
-                    break;
-                  case "Sweden":
-                    return `#7ed957`;
-                    break;
-                  case "Finland":
-                    return `#7ed957`;
-                    break;
-                  case "Denmark":
-                    return `#7ed957`;
-                    break;
-                  case "Portugal":
-                    return `#7ed957`;
-                    break;
-                  case "United Kingdom":
-                    return `#7ed957`;
-                    break;
-                  case "Iceland":
-                    return `#7ed957`;
-                    break;
-                  case "United States of America":
-                    return `#7ed957`;
-                    break;
-                  case "Cuba":
-                    return `#7ed957`;
-                    break;
-                  case "Australia":
-                    return `#7ed957`;
-                    break;
-                  case "Canada":
-                    return `#7ed957`;
-                    break;
-                  case "Estonia":
-                    return `#7ed957`;
-                    break;
-                  case "United Arab Emirates":
-                    return `#7ed957`;
-                    break;
-                  case "Saudi Arabia":
-                    return `#7ed957`;
-                    break;
-                  case "Israel":
-                    return `#7ed957`;
-                    break;
-                  case "Singapore":
-                    return `#7ed957`;
-                    break;
-                  case "Thailand":
-                    return `#7ed957`;
-                    break;
-                  case "South Korea":
-                    return `#7ed957`;
-                    break;
-                  case "Japan":
-                    return `#7ed957`;
-                    break;
-                  case "Indonesia":
-                    return `#7ed957`;
-                    break;
-                  case "Brazil":
-                    return `#7ed957`;
-                    break;
-                  case "Mexico":
-                    return `#7ed957`;
-                    break;
-                  case "Chile":
-                    return `#7ed957`;
-                    break;
-                  case "Argentina":
-                    return `#7ed957`;
-                    break;
-                  case "Peru":
-                    return `#7ed957`;
-                    break;
-                  case "Taiwan":
-                    return `#7ed957`;
-                    break;
-                  case "New Zealand":
-                    return `#7ed957`;
-                    break;
-                  case "Vietnam":
-                    return `#7ed957`;
-                    break;
-                  case "Nepal":
-                    return `#7ed957`;
-                    break;
-                  case "Bhutan":
-                    return `#7ed957`;
-                    break;
-                  default:
-                    return `#FF0000`;
-                }
+                return ['#B50EAE', '#CC1C71', '#D92350', '#E32A34'][Math.round(Math.random() * 3)]
+                // switch (x.properties.SOVEREIGNT) {
+                //   case "India":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Russia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "France":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Belgium":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Spain":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Germany":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Poland":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Ireland":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Italy":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Greece":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Croatia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "South Africa":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Austria":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Switzerland":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Netherlands":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Norway":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Sweden":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Finland":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Denmark":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Portugal":
+                //     return `#7ed957`;
+                //     break;
+                //   case "United Kingdom":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Iceland":
+                //     return `#7ed957`;
+                //     break;
+                //   case "United States of America":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Cuba":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Australia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Canada":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Estonia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "United Arab Emirates":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Saudi Arabia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Israel":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Singapore":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Thailand":
+                //     return `#7ed957`;
+                //     break;
+                //   case "South Korea":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Japan":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Indonesia":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Brazil":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Mexico":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Chile":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Argentina":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Peru":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Taiwan":
+                //     return `#7ed957`;
+                //     break;
+                //   case "New Zealand":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Vietnam":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Nepal":
+                //     return `#7ed957`;
+                //     break;
+                //   case "Bhutan":
+                //     return `#7ed957`;
+                //     break;
+                //   default:
+                //     return `#FF0000`;
+                // }
               }}
             />
           </Col>
           <Col md={true} className="align-self-center order-md-first">
-            <h1 className={`${styles.MountainHeading} text-center`}>
-              <span>
+            <h1 className={`${styles.MountainHeading}`} >
+            The answer to all your travel problems
+              {/* <span>
                 Our service is{" "}
                 <span className={styles.underlineGreen}>
                   Operational and Live everywhere
                 </span>{" "}
                 <span className={styles.underline}></span>
-              </span>
+              </span> */}
             </h1>
           </Col>
         </Row>
