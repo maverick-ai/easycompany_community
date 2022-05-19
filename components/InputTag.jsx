7fce6dc9c01ce324bbf386d6f27738d14d0b457c
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2022-05-19 19:57:29 +0530

    card updated

--- components/InputTag.jsx
+++ components/InputTag.jsx
@@ -1,67 +1,66 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./tagsinput.module.css";
 
 function TagsInput(props) {
+  const setInput = props.changeInput;
+  const setTags = props.changeTags;
+  const tags = props.tags;
+  const input = props.input;
+  const [isKeyReleased, setIsKeyReleased] = useState(false);
 
-    const setInput = props.changeInput;
-    const setTags = props.changeTags;
-    const tags = props.tags;
-    const input = props.input;
-    const [isKeyReleased, setIsKeyReleased] = useState(false);
+  const onChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
 
-    const onChange = (e) => {
-        const { value } = e.target;
-        setInput(value);
-      };
+  const onKeyDown = (e) => {
+    const { key } = e;
+    const trimmedInput = input.trim();
 
-    const onKeyDown = (e) => {
-        const { key } = e;
-        const trimmedInput = input.trim();
-      
-        if (key === ' ' && trimmedInput.length && !tags.includes(trimmedInput)) {
-          e.preventDefault();
-          setTags(prevState => [...prevState, trimmedInput]);
-          setInput('');
-        }
-      
-        if (key === "Backspace" && !input.length && tags.length && isKeyReleased) {
-          const tagsCopy = [...tags];
-          const poppedTag = tagsCopy.pop();
-          e.preventDefault();
-          setTags(tagsCopy);
-          setInput(poppedTag);
-        }
-      
-        setIsKeyReleased(false);
-    };
-      
-    const onKeyUp = () => {
+    if (key === " " && trimmedInput.length && !tags.includes(trimmedInput)) {
+      e.preventDefault();
+      setTags((prevState) => [...prevState, trimmedInput]);
+      setInput("");
+    }
+
+    if (key === "Backspace" && !input.length && tags.length && isKeyReleased) {
+      const tagsCopy = [...tags];
+      const poppedTag = tagsCopy.pop();
+      e.preventDefault();
+      setTags(tagsCopy);
+      setInput(poppedTag);
+    }
+
+    setIsKeyReleased(false);
+  };
+
+  const onKeyUp = () => {
     setIsKeyReleased(true);
-    }
+  };
 
-    const deleteTag = (index) => {
-    setTags(prevState => prevState.filter((tag, i) => i !== index))
-    }
+  const deleteTag = (index) => {
+    setTags((prevState) => prevState.filter((tag, i) => i !== index));
+  };
 
-
-    return (
+  return (
     <div className={styles.containerOutside}>
       <div className={styles.container}>
-      {tags.map((tag, index) => (
+        {tags.map((tag, index) => (
           <div className={styles.tag} key={index}>
-              {tag}
-              <button onClick={() => deleteTag(index)}>x</button>
+            {tag}
+            <button onClick={() => deleteTag(index)}>x</button>
           </div>
-      ))}
-      <input
+        ))}
+        <input
           value={input}
           className={styles.InputTags}
           placeholder="Enter a tag"
           onKeyDown={onKeyDown}
           onKeyUp={onKeyUp}
           onChange={onChange}
-          />
+        />
+      </div>
     </div>
-  </div>);
+  );
 }
-export default TagsInput; 
+export default TagsInput;
--- pages/questions.js
+++ pages/questions.js
@@ -1,74 +1,262 @@
-import styles from "../styles/Questions.module.css";
-import { PostListURL, PageSize } from "../components/constants"
-import Link from 'next/link'
-import Pagination from '../components/Pagination';
-import Router from 'next/router'
-import {sendReq} from "../components/requests";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import {faArrowAltCircleUp,faArrowAltCircleDown} from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/Questions.module.scss";
+import { PostListURL, PageSize } from "../components/constants";
+import Link from "next/link";
+import Pagination from "../components/Pagination";
+import { sendReq } from "../components/requests";
 
+const Questions = ({ questions, query }) => {
+  return (
+    <div className={`${styles.questionsList} container`}>
+      {questions.count != 0 &&
+        questions.results.map((data) => (
+          //   <Link href={`/posts/?postid=${data.pk}&page=1`}>
+          //     <div className={`row ${styles.qcard}`} key={data.pk}>
+          //       <div className={`col-3 col-sm-2 ${styles.voteCol}`}>
+          //         <FontAwesomeIcon
+          //           className={styles.upVoteIcon}
+          //           icon={faArrowAltCircleUp}
+          //         />
+          //         <p className={styles.voteText}>
+          //           {data.upVoteNumber - data.downVoteNumber}
+          //         </p>
+          //         <FontAwesomeIcon
+          //           className={styles.downVoteIcon}
+          //           icon={faArrowAltCircleDown}
+          //         />
+          //       </div>
 
-const Questions = ({questions, query}) => {
- 
-    return (
-        <div className={`${styles.questionsList} container`}>
-        {questions.count!=0 &&
-            questions.results.map((data) => (
-            <Link href={`/posts/?postid=${data.pk}&page=1`}><div className={`row ${styles.qcard}`} key={data.pk}>
-            <div className={`col-3 col-sm-2 ${styles.voteCol}`}>
-                <FontAwesomeIcon className={styles.upVoteIcon} icon={faArrowAltCircleUp} />
-                <p className={styles.voteText}>{data.upVoteNumber - data.downVoteNumber}</p>
-                <FontAwesomeIcon className={styles.downVoteIcon} icon={faArrowAltCircleDown}/>
+          //       <div className={`col-9 col-sm-10 ${styles.qbox}`}>
+          //         <p className={styles.title}>
+          //           <strong>
+          //             {" "}
+          //             {data.title.length > 60
+          //               ? data.title.substring(0, 60) + " ..."
+          //               : data.title}
+          //           </strong>
+          //         </p>
+          //         <div className={`category-list ${styles.catlist}`}>
+          //           {data.categoryOfThePost.map((category) => (
+          //             <p
+          //               className={styles.category}
+          //               key={category.categoryOfThePost}
+          //             >
+          //               {category.categoryForPost}
+          //             </p>
+          //           ))}
+          //         </div>
+          //       </div>
+          //     </div>
+          //   </Link>
+          <>
+            <h1>Top Questions</h1>
+            <div className={styles.wrapper}>
+              <div className={styles.left}>
+                <Link
+                  href={`/posts/?postid=${data.pk}&page=1`}
+                  className={styles.title}
+                >
+                  {data.title.length > 60
+                    ? data.title.substring(0, 60) + " ..."
+                    : data.title}
+                </Link>
+                <ul>
+                  <li>Mangalore</li>
+                  <li>Karnataka</li>
+                  <li>Tourist Spots</li>
+                  <li>India </li>
+                </ul>
+              </div>
+              <div className={styles.right}>
+                <ul>
+                  <li>
+                    5 Votes{" "}
+                    <img
+                      src="/icons/arrow_upward.svg"
+                      alt="upward arrow icon"
+                    />{" "}
+                  </li>
+                  <li>
+                    1 Answer <img src="/icons/answer.svg" alt="answer icon" />
+                  </li>
+                  <li>
+                    7 Views <img src="/icons/eye.svg" alt="eye icon" />
+                  </li>
+                </ul>
+              </div>
             </div>
-
-            <div className={`col-9 col-sm-10 ${styles.qbox}`}>
-                <p className={styles.title}><strong> {data.title.length>60?data.title.substring(0,60) + " ...": data.title}</strong></p>
-                <div className={`category-list ${styles.catlist}`}>
-                    {data.categoryOfThePost.map((category)=> (
-                        <p className={styles.category} key={category.categoryOfThePost}>{category.categoryForPost}</p>
-                    ))}
-                </div>
+            <div className={styles.wrapper}>
+              <div className={styles.left}>
+                <Link
+                  href={`/posts/?postid=${data.pk}&page=1`}
+                  className={styles.title}
+                >
+                  {data.title.length > 60
+                    ? data.title.substring(0, 60) + " ..."
+                    : data.title}
+                </Link>
+                <ul>
+                  <li>Mangalore</li>
+                  <li>Karnataka</li>
+                  <li>Tourist Spots</li>
+                  <li>India </li>
+                </ul>
+              </div>
+              <div className={styles.right}>
+                <ul>
+                  <li>
+                    5 Votes{" "}
+                    <img
+                      src="/icons/arrow_upward.svg"
+                      alt="upward arrow icon"
+                    />{" "}
+                  </li>
+                  <li>
+                    1 Answer <img src="/icons/answer.svg" alt="answer icon" />
+                  </li>
+                  <li>
+                    7 Views <img src="/icons/eye.svg" alt="eye icon" />
+                  </li>
+                </ul>
+              </div>
             </div>
-                
-            </div></Link>
-        ))
-        }
-        {questions.count > PageSize && 
-        <Pagination 
-        
-            currentPage={query.page}
-            count={Math.ceil(questions.count/PageSize)}
-            baseURL={`/questions?`}
-            
-        />}
-        </div>
-    )
-}
+            <div className={styles.wrapper}>
+              <div className={styles.left}>
+                <Link
+                  href={`/posts/?postid=${data.pk}&page=1`}
+                  className={styles.title}
+                >
+                  {data.title.length > 60
+                    ? data.title.substring(0, 60) + " ..."
+                    : data.title}
+                </Link>
+                <ul>
+                  <li>Mangalore</li>
+                  <li>Karnataka</li>
+                  <li>Tourist Spots</li>
+                  <li>India </li>
+                </ul>
+              </div>
+              <div className={styles.right}>
+                <ul>
+                  <li>
+                    5 Votes{" "}
+                    <img
+                      src="/icons/arrow_upward.svg"
+                      alt="upward arrow icon"
+                    />{" "}
+                  </li>
+                  <li>
+                    1 Answer <img src="/icons/answer.svg" alt="answer icon" />
+                  </li>
+                  <li>
+                    7 Views <img src="/icons/eye.svg" alt="eye icon" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.wrapper}>
+              <div className={styles.left}>
+                <Link
+                  href={`/posts/?postid=${data.pk}&page=1`}
+                  className={styles.title}
+                >
+                  {data.title.length > 60
+                    ? data.title.substring(0, 60) + " ..."
+                    : data.title}
+                </Link>
+                <ul>
+                  <li>Mangalore</li>
+                  <li>Karnataka</li>
+                  <li>Tourist Spots</li>
+                  <li>India </li>
+                </ul>
+              </div>
+              <div className={styles.right}>
+                <ul>
+                  <li>
+                    5 Votes{" "}
+                    <img
+                      src="/icons/arrow_upward.svg"
+                      alt="upward arrow icon"
+                    />{" "}
+                  </li>
+                  <li>
+                    1 Answer <img src="/icons/answer.svg" alt="answer icon" />
+                  </li>
+                  <li>
+                    7 Views <img src="/icons/eye.svg" alt="eye icon" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.wrapper}>
+              <div className={styles.left}>
+                <Link
+                  href={`/posts/?postid=${data.pk}&page=1`}
+                  className={styles.title}
+                >
+                  {data.title.length > 60
+                    ? data.title.substring(0, 60) + " ..."
+                    : data.title}
+                </Link>
+                <ul>
+                  <li>Mangalore</li>
+                  <li>Karnataka</li>
+                  <li>Tourist Spots</li>
+                  <li>India </li>
+                </ul>
+              </div>
+              <div className={styles.right}>
+                <ul>
+                  <li>
+                    5 Votes{" "}
+                    <img
+                      src="/icons/arrow_upward.svg"
+                      alt="upward arrow icon"
+                    />{" "}
+                  </li>
+                  <li>
+                    1 Answer <img src="/icons/answer.svg" alt="answer icon" />
+                  </li>
+                  <li>
+                    7 Views <img src="/icons/eye.svg" alt="eye icon" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </>
+        ))}
+      {questions.count > PageSize && (
+        <Pagination
+          currentPage={query.page}
+          count={Math.ceil(questions.count / PageSize)}
+          baseURL={`/questions?`}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Questions;
 
 export async function getServerSideProps({ query }) {
-    if (!query.page) 
-        query.page=1;
-    let questions=[]
-    try{
-        questions = await sendReq(`${PostListURL}?page=${query.page}`)
-    }
-
-    catch(err){
-        return {
-            redirect: {
-                destination: "/404",
-                permanent: false,
-            }
-        }
-    }
-
+  if (!query.page) query.page = 1;
+  let questions = [];
+  try {
+    questions = await sendReq(`${PostListURL}?page=${query.page}`);
+  } catch (err) {
     return {
-        props: {
-            questions,
-            query,
-        }
-    }
-    
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      questions,
+      query,
+    },
+  };
 }
