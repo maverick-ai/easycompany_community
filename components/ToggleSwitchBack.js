9a5e50978a6c50d82f6329c8c97e9aee31b65f45
abhay <joymamgain@gmail.com>
2022-07-14 17:39:19 +0530

    login signup fix

--- components/ToggleSwitchBack.js
+++ components/ToggleSwitchBack.js
@@ -4,16 +4,18 @@ import Link from "next/link";
 
 const ToggleSwitchToSignUp = () => {
   return (
-    <div className={styles.container} style={{transform:"rotate(-90deg)"}}>
+    <div className={styles.container} style={{ transform: "rotate(-90deg)" }}>
       <div className={styles.toggle_switch}>
-        <input type="checkbox" className={styles.checkbox}
-          />
+        <input type="checkbox" className={styles.checkbox} />
         <label className={styles.label}>
           <span className={styles.inner} />
-          <Link style={{ textDecoration: "none" }} href={"/login_new"}>
-            <span className={styles.switch}><div style={{ marginTop: "100px", transform: "rotate(90deg)" }}>
-              <span className={styles.RegisterLink}>Log In</span>
-            </div></span></Link>
+          <Link style={{ textDecoration: "none" }} href={"/login"}>
+            <span className={styles.switch}>
+              <div style={{ marginTop: "100px", transform: "rotate(90deg)" }}>
+                <span className={styles.RegisterLink}>Log In</span>
+              </div>
+            </span>
+          </Link>
         </label>
       </div>
     </div>
