dd7fb2efda33ae31f848fcb3613221e9736ceb3b
abhay <joymamgain@gmail.com>
2022-07-15 21:46:37 +0530

    detailed post fix bug

--- components/DetailedPost.jsx
+++ components/DetailedPost.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { sendVote, addpostcomment, sendReq } from "./requests";
-import { UpVotePostURL, DownVotePostURL } from "./constants";
+import { UpVotePostURL, DownVotePostURL, commentPageSize } from "./constants";
 import PostComment from "./PostComments";
 import Link from "next/link";
 import { useEffect, useState } from "react";
