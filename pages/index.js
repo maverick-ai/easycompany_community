7383508b3be5fe951d23e864e23d83c15d784039
Sarthak Bansal <sbansal793@gmail.com>
2022-08-19 19:30:44 +0530

    added fonts and resolved phone

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from "react";
 import Page1 from "../components/page1";
 import Page2 from "../components/phonepage";
@@ -6,21 +7,21 @@ import Page2 from "../components/phonepage";
 export default function Home() {
   const [winWidth, setWinWidth] = useState(true);
  
-  // useEffect(() => {
+  useEffect(() => {
   
-  //   if (window.innerWidth <= 768) {
-  //     setWinWidth(false);
-  //   } else {
-  //     setWinWidth(true);
-  //   }
-  //   window.addEventListener("resize", () => {
-  //     if (window.innerWidth <= 768) {
-  //       setWinWidth(false);
-  //     } else {
-  //       setWinWidth(true);
-  //     }
-  //   });
-  // }, []);
+    if (window.innerWidth <= 768) {
+      setWinWidth(false);
+    } else {
+      setWinWidth(true);
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth <= 768) {
+        setWinWidth(false);
+      } else {
+        setWinWidth(true);
+      }
+    });
+  }, []);
 
-  return  <Page2 />;
+  return winWidth ? <Page1 /> : <Page2 />;
 }
