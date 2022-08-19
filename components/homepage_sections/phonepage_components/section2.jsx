018fbe44667bffff19df6a68b03062ba5b55d959
Sarthak Bansal <sbansal793@gmail.com>
2022-08-19 23:38:16 +0530

    made changes to the animation

--- components/homepage_sections/phonepage_components/section2.jsx
+++ components/homepage_sections/phonepage_components/section2.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import styles from "../../../styles/Homepage/mobileCSS/section2.module.scss";
-import { Col, Row } from "react-bootstrap";
+import Section2Carousel from "./section2_carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-multi-carousel/lib/styles.css";
 // import Carousel_2 from "./homepage_sections/phonepage_components/carousel_2";
 
 
 function Section2() {
-    return <Row style={{ marginBottom: "5%" }}>
-        <Col xl={5} style={{ padding: "0 20px" }}>
-            <div>
-                <span className={styles.p3h}>
-                    Simplify your <span className={styles.love}>travel</span> like{" "}
-                    <br /> never before.
-                </span>
+    return <div><div className={styles.Section2Div}>
+                <h2 className={styles.section2Heading}>
+                    Simplify your <span className={styles.love}>travel</span>
+                    <br />like never before.<img className={styles.section2headingDoodle} src="phone_assets/section2headingDoodle.png" />
+                </h2>
                 <div className={styles.p2div}>
                     <p className={styles.p2}>
                         Planning a trip to somewhere new? Solo or in a group? Looking
@@ -22,11 +20,9 @@ function Section2() {
                         for your hostel stay in New Zealand.
                     </p>
                 </div>
-            </div>
-        </Col>
-
-        {/* <Carousel_2 /> */}
-    </Row>
+    </div>
+    <Section2Carousel />
+    </div>
 }
 
 export default Section2;
