aad759b52b80f04d557335cb79604598930dad67
Sarthak Bansal <sbansal793@gmail.com>
2022-08-19 20:47:41 +0530

    section 1 completed

--- components/homepage_sections/phonepage_components/section1.jsx
+++ components/homepage_sections/phonepage_components/section1.jsx
@@ -6,12 +6,12 @@ function Section1() {
     <div>
         <div>
           <h1 className={styles.text1}>
-            FIND THE BEST<span><img className={styles.paperPlane} src="paperplaneHomepage.png"/></span>
+            FIND THE BEST<span><img className={styles.paperPlane} src="phone_assets/paperplaneHomepage.png"/></span>
             <br/>
             <div style={{ position: "relative", display: "inline" }}>
               <span className={styles.answers}>ANSWERS</span>
               <img
-                src="photos/underline.png"
+                src="phone_assets/underline.png"
                 className={styles.underline}
               ></img>
             </div>{" "}
@@ -20,14 +20,14 @@ function Section1() {
           </h1>
           
               <button className={styles.join}>
-                <img src="photos/Vector.png" className={styles.vector}></img>
-                <img src="photos/Group11.png" className={styles.Group11}></img>
+                <img src="phone_assets/Vector.png" className={styles.vector}></img>
+                <img src="phone_assets/Group11.png" className={styles.Group11}></img>
                 <p className={styles.joinText}>JOIN THE COMMUNITY</p>
               </button>
        
         </div>
         <div style={{ textAlign: "center" }}>
-          <img src="photos/Group30.png" className={styles.mainImg}></img>
+          <img src="phone_assets/landingpage_main.png" className={styles.mainImg}></img>
         </div>
       </div>
   );
