a245eb2ecc5e4d8b2cebff76ecc4a18e20529e19
Sarthak Bansal <sbansal793@gmail.com>
2022-08-20 16:22:16 +0530

    made changes to the section3

--- components/homepage_sections/phonepage_components/section3.jsx
+++ components/homepage_sections/phonepage_components/section3.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../../../styles/Homepage/mobileCSS/section3.module.scss";
+import Section3Photos from "./section3_photos";
 
 function Section3() {
   return (
@@ -17,88 +18,7 @@ function Section3() {
           <img src="phone_assets/Section3Plane.png" className={styles.arrl} />
         </h2>
       </div>
-
-      {/* photos */}
-      <div style={{ margin: "0 auto", width: "300px" }}>
-        <div
-          style={{
-            textAlign: "center",
-            marginTop: "2%",
-            position: "relative",
-
-            display: "flex",
-          }}
-        >
-          <img src="photos/plane.png" className={styles.plane} />
-          <div className={styles.carddiv}>
-            <img src="photos/t1.svg" className={styles.card} />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                textAlign: "center",
-                justifySelf: "center",
-                justifyContent: "center",
-                marginTop: "10px",
-              }}
-            >
-              <div style={{ position: "relative" }}>
-                <img src="photos/tri.svg" className={styles.tri} />
-                <div className={styles.ques}>
-                  <p className={styles.questext}>
-                    Solo or in a group? Need advice?
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.carddiv}>
-            <img src="photos/t2.svg" className={styles.card} />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                textAlign: "center",
-                justifySelf: "center",
-                justifyContent: "center",
-                marginTop: "10px",
-              }}
-            >
-              <div style={{ position: "relative" }}>
-                <img src="photos/tri.svg" className={styles.tri} />
-                <div className={styles.ques}>
-                  <p className={styles.questext}>
-                    Planning a trip to somewhere new?
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.carddiv}>
-            <img src="photos/t3.svg" className={styles.card} />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                textAlign: "center",
-                justifySelf: "center",
-                justifyContent: "center",
-                marginTop: "10px",
-              }}
-            >
-              <div style={{ position: "relative" }}>
-                <img src="photos/tri.svg" className={styles.tri} />
-                <div className={styles.ques}>
-                  <p className={styles.questext}>
-                    Looking for recommendations?
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <img src="photos/bag1.png" className={styles.bag1}></img>
-        </div>
-      </div>
+      <Section3Photos />
     </div>
   );
 }
---
+++ components/homepage_sections/phonepage_components/section3_photos.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "../../../styles/Homepage/mobileCSS/section3Photos.module.scss";
+
+function Section3Photos() {
+  return (
+    <div className={styles.Section3PhotosDiv}>
+          <div className={styles.carddiv}>
+             <img src="phone_assets/Section3Aeroplane.png" className={styles.plane} />
+            <img src="phone_assets/Section3Photo1.png"/>
+            <div
+              className={styles.Section3PhotosTextComponent}
+            >
+                <div className={styles.ques}>
+                <img src="phone_assets/tri.svg" className={styles.tri} />
+                  <p className={styles.questext}>
+                    Solo or in a group? Need advice?
+                  </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.carddiv}>
+            <img src="phone_assets/Section3Photo2.png"/>
+            <div
+              className={styles.Section3PhotosTextComponent}
+            >
+                <div className={styles.ques}>
+                <img src="phone_assets/tri.svg" className={styles.tri} />
+                  <p className={styles.questext}>
+                  Planning a trip to somewhere new?
+                  </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.carddiv}>
+            <img src="phone_assets/Section3Photo3.png"/>
+            <div
+              className={styles.Section3PhotosTextComponent}
+            >
+                <div className={styles.ques}>
+                <img src="phone_assets/tri.svg" className={styles.tri} />
+                  <p className={styles.questext}>
+                  Looking for recommendations?
+                  </p>
+              </div>
+            </div>
+          </div>
+          {/* <img src="photos/bag1.png" className={styles.bag1}></img> */}
+    </div>
+  );
+}
+
+export default Section3Photos;
