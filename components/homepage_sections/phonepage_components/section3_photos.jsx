53a777fc3e0f9aa9bd7b194c8540135887590c84
Sarthak Bansal <sbansal793@gmail.com>
2022-08-20 22:12:46 +0530

    section 3 completed

--- components/homepage_sections/phonepage_components/section3_photos.jsx
+++ components/homepage_sections/phonepage_components/section3_photos.jsx
@@ -1,51 +1,16 @@
 import React from "react";
 import styles from "../../../styles/Homepage/mobileCSS/section3Photos.module.scss";
+import Marquee from "react-fast-marquee";
+import Section3PhotosComponent from "./section3_photos_component";
 
 function Section3Photos() {
   return (
-    <div className={styles.Section3PhotosDiv}>
-          <div className={styles.carddiv}>
-             <img src="phone_assets/Section3Aeroplane.png" className={styles.plane} />
-            <img src="phone_assets/Section3Photo1.png"/>
-            <div
-              className={styles.Section3PhotosTextComponent}
-            >
-                <div className={styles.ques}>
-                <img src="phone_assets/tri.svg" className={styles.tri} />
-                  <p className={styles.questext}>
-                    Solo or Budget?Need advice
-                  </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.carddiv}>
-            <img src="phone_assets/Section3Photo2.png"/>
-            <div
-              className={styles.Section3PhotosTextComponent}
-            >
-                <div className={styles.ques}>
-                <img src="phone_assets/tri.svg" className={styles.tri} />
-                  <p className={styles.questext}>
-                  Planning a trip to somewhere?
-                  </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.carddiv}>
-             <img src="phone_assets/Section3Bag.png" className={styles.bag}/>
-            <img src="phone_assets/Section3Photo3.png"/>
-            <div
-              className={styles.Section3PhotosTextComponent}
-            >
-                <div className={styles.ques}>
-                <img src="phone_assets/tri.svg" className={styles.tri} />
-                  <p className={styles.questext}>
-                  Looking for help, info, review ?
-                  </p>
-              </div>
-            </div>
-          </div>
-    </div>
+    <Marquee loop={0} gradient={false} direction="right">
+      <Section3PhotosComponent imgSrc="phone_assets/Section3Photo1.png"  text="Solo or in a group ? Need advice"/>
+      <Section3PhotosComponent imgSrc="phone_assets/Section3Photo2.png"  text="Planning a trip to somewhere ?"/>
+      <Section3PhotosComponent imgSrc="phone_assets/Section3Photo3.png"  text="Looking for recommendation ?"/>
+      <Section3PhotosComponent imgSrc="phone_assets/Section3Photo4.png"  text="Not able to find good restuarants ?"/>
+    </Marquee>
   );
 }
 
