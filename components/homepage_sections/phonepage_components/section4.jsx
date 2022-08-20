73660b7d99880f428333261976d61fc55d7601d6
Sarthak Bansal <sbansal793@gmail.com>
2022-08-20 17:41:32 +0530

    completed section 4

---
+++ components/homepage_sections/phonepage_components/section4.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "../../../styles/Homepage/mobileCSS/section4.module.scss";
+import { Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// function Section4() {
+//   return (
+//     <Row>
+//       <Col>
+//         <Row>
+//           <div className={styles.outer}>
+//             <p className={styles.fontstyleouter}>
+//               <span className={styles.fontstyleinner}>Easycommunity</span> has
+//               answers to all of your questions! <br />
+//               And if it doesn't yet ? Well,
+//               <br />
+//               <span className={styles.fontstyleinner}> Just Ask!</span>
+//             </p>
+//             <img src="photos/suitec.svg" className={styles.suit}></img>
+//           </div>
+//         </Row>
+//       </Col>
+//       <Col >
+//         <img src="photos/bigglobe.svg" className={styles.globe}></img>
+//       </Col>
+//     </Row>
+//   );
+// }
+
+function Section4() {
+    return (
+        <div className={styles.Section4Parent}>
+            <div className={styles.Section4FlexDiv}>
+            <h2 className={styles.fontstyleouter}>
+              <span className={styles.fontstyleinner}>Easycommunity</span>  <br />
+              has answers to all<br /> of your questions! <br />
+              And if it doesn't yet? Well,
+              <br />
+              <span className={styles.fontstyleinner}><img src="phone_assets/Section4Suitcase.png" className={styles.suit}/> Just Ask!</span>
+            </h2>
+            </div>
+            <div className={styles.Section4FlexDiv}>
+            <img src="photos/bigglobe.svg" className={styles.globe}/>
+            </div>
+        </div>
+    );
+  }
+
+
+
+export default Section4;
--- components/phonepage.js
+++ components/phonepage.js
@@ -9,57 +9,57 @@ import Carousel_row from "./homepage_sections/phonepage_components/carousel _row
 import Section1 from "./homepage_sections/phonepage_components/section1";
 import Section2 from "./homepage_sections/phonepage_components/section2";
 import Section3 from "./homepage_sections/phonepage_components/section3";
-import Phone3 from "./homepage_sections/phonepage_components/thirdpage_phone";
+import Section4 from "./homepage_sections/phonepage_components/section4";
 import Phone4 from "./homepage_sections/phonepage_components/fourthpage_phone";
 import Phone6 from "./homepage_sections/phonepage_components/sixthpage_phone";
 import Phone7 from "./homepage_sections/phonepage_components/seventhpage_phone";
 import We_are from "./homepage_sections/phonepage_components/we_are";
+
 function Page2() {
   return (
     <div>
-    <Container>
-      <Section1 />
-      <Section2 />
+      <Container>
+        <Section1 />
+        <Section2 />
         <Section3 />
-    <Row>
-        <Phone4 />
-      </Row>
-      <Carousel_row />
-      <We_are />
-      <Row>
-        <Phone6 />
-      </Row>
-      <div>
-        <div style={{ position: "relative", height: "150px" }}>
-          <img src="photos/polar1.png" className={styles.polar1}></img>
+        <Section4 />
+        <Carousel_row />
+        <We_are />
+        <Row>
+          <Phone6 />
+        </Row>
+        <div>
+          <div style={{ position: "relative", height: "150px" }}>
+            <img src="photos/polar1.png" className={styles.polar1}></img>
 
-          <img src="photos/polar2.png" className={styles.polar2}></img>
+            <img src="photos/polar2.png" className={styles.polar2}></img>
 
-          <img src="photos/polar3.png" className={styles.polar3}></img>
+            <img src="photos/polar3.png" className={styles.polar3}></img>
 
-          <img src="photos/polar4.png" className={styles.polar4}></img>
-        </div>
-      </div>
-      <Row>
-        <Col sm={10} xs={10}>
-          <div className={styles.white}>
-            <p className={styles.whitep}>
-              <span className={styles.whites}>Easycommunity</span> offers a vast
-              collection of travel-related questions and answers. Improvise your
-              dream trip by asking us your queries about new and offbeat places,
-              food recommendations, or anything else. Find the best answer to
-              your travel questions or help others get their questions answered!
-            </p>
-
-            <img
-              src="photos/hotAirBalloon.png"
-              className={styles.hotAirBalloon}
-            ></img>
+            <img src="photos/polar4.png" className={styles.polar4}></img>
           </div>
-        </Col>
-      </Row>
-      <Phone7 />
-    </Container>
+        </div>
+        <Row>
+          <Col sm={10} xs={10}>
+            <div className={styles.white}>
+              <p className={styles.whitep}>
+                <span className={styles.whites}>Easycommunity</span> offers a
+                vast collection of travel-related questions and answers.
+                Improvise your dream trip by asking us your queries about new
+                and offbeat places, food recommendations, or anything else. Find
+                the best answer to your travel questions or help others get
+                their questions answered!
+              </p>
+
+              <img
+                src="photos/hotAirBalloon.png"
+                className={styles.hotAirBalloon}
+              ></img>
+            </div>
+          </Col>
+        </Row>
+        <Phone7 />
+      </Container>
     </div>
 
     // <div className={s.page}>
